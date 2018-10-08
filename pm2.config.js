f301cb255ab28ce2b5ebc587c7ec7d9325a37f37
v_yguiliu <v_yguiliu@tencent.com>
2018-10-08 16:06:38 +0800

    fix eslint

--- pm2.config.js
+++ pm2.config.js
@@ -5,12 +5,12 @@ module.exports = {
       script: 'dest/index.js',
       // watch: true,
       env: {
-        "NODE_ENV": "production",
+        'NODE_ENV': 'production'
       },
       // instances: 4,
       // exec_mode: "cluster",
       // 日志时间格式
-      log_date_format: "YYYY-MM-DD HH:mm:ss.SSS"
+      log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS'
     }
   ]
 }
